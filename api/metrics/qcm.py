b296da012b48c9f108b99e8553f124079fd0dc57
dtrckd <dtrckd@gmail.com>
2024-11-03 23:04:18 +0100

    feat(metric): add qcm metric

--- api/metrics/qcm.py
+++ api/metrics/qcm.py
@@ -3,4 +3,11 @@ from . import metric_registry
 
 @metric_registry.register(name="qcm_exactness", metric_type="llm", require=["output", "output_true"])
 def qcm_metric(output, output_true, **kwargs):
-    pass
+    observation = output.strip(".'\" \n")
+    observation = observation.split()
+    if len(observation) > 1:
+        return None
+
+    observation =  output[0]
+    score = 1 if observation == output_true else 0
+    return score
