af1c7f3afbc645c26cd65c7e2025f05ac9a698d3
dtrckd <dtrckd@gmail.com>
2024-11-15 20:32:40 +0100

    fix: remove nb_tokens_* attribute for ObservationTable + use ops metric type.

--- api/metrics/generation_ops_metric.py
+++ api/metrics/generation_ops_metric.py
@@ -1,19 +1,21 @@
 from . import metric_registry
 
+
 @metric_registry.register(
     name="nb_tokens_prompt",
     description="Number of tokens in the prompt",
-    metric_type="llm",
+    metric_type="ops",
     require=["query"],
 )
 def nb_tokens_prompt_metric(output, *args, **kwargs):
     metadata = kwargs["metadata"]
     return metadata.get("nb_tokens_prompt")
 
+
 @metric_registry.register(
     name="nb_tokens_completion",
     description="Number of tokens in the completion",
-    metric_type="llm",
+    metric_type="ops",
     require=["query"],
 )
 def nb_tokens_completion_metric(output, *args, **kwargs):
@@ -24,7 +26,7 @@ def nb_tokens_completion_metric(output, *args, **kwargs):
 @metric_registry.register(
     name="generation_time",
     description="The time to generate the answer/output",
-    metric_type="llm",
+    metric_type="ops",
     require=["query"],
 )
 def generation_time_metric(output, *args, **kwargs):
