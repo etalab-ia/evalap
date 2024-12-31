7dbc96d324b94a8b90ccd8e7a7b4e08eff4d70a2
Audrey_CLEVY <audrey_clevy@airdeaureyclevy.home>
2024-12-31 10:16:43 +0100

    add css style

--- ui/demo_streamlit/app.py
+++ ui/demo_streamlit/app.py
@@ -1,17 +1,17 @@
 import streamlit as st
 from routes import ROUTES, get_page
+from utils import load_css
 
+# Configuration 
+st.set_page_config(
+    page_title="EG1 - Évaluation",
+    page_icon="ui/demo_streamlit/static/images/eg1_logo.png", 
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+load_css("style.css")
 
 pg = st.navigation([get_page(route) for route in ROUTES])
-st.set_page_config(layout="wide")
-custom_css = """
-<style>
-* {
-    font-size: 18px;
-}
-</style>
-"""
-st.markdown(custom_css, unsafe_allow_html=True)
-
 
 pg.run()
