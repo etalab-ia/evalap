6377af4d241245384b1e02beabd554e528f67ba5
dtrckd <dtrckd@gmail.com>
2025-02-26 23:58:02 +0100

    fix tests

--- api/tests/test_api.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy.orm import Session
-
-class TestApi:
-    """Base test class that all test classes should inherit from."""
-    pass  # Database setup is now handled by fixtures in conftest.py
--- ui/demo_streamlit/utils.py
+++ ui/demo_streamlit/utils.py
@@ -13,7 +13,7 @@ def fetch(method, endpoint, data=None):
     elif data:
         kw["json"] = data
 
-        response = func(f"{API_BASE_URL}{endpoint}{q}", **kw)
+    response = func(f"{API_BASE_URL}{endpoint}{q}", **kw)
     if response.status_code == 200:
         return response.json()
     else:
