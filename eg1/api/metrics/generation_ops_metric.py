47eb1142392ecf58c6e23497e0e931d14894e153
Audrey_CLEVY <audrey_clevy@airdeaureyclevy.home>
2025-04-18 11:10:59 +0200

    fix

--- eg1/api/metrics/generation_ops_metric.py
+++ eg1/api/metrics/generation_ops_metric.py
@@ -13,17 +13,18 @@ def nb_tokens_prompt_metric(output, *args, **kwargs):
     name="nb_tokens_completion",
     description="Number of tokens in the completion",
     metric_type="ops",
-    require=["query"],
+    require=["output"],
 )
 def nb_tokens_completion_metric(output, *args, **kwargs):
     metadata = kwargs["metadata"]
     return metadata.get("nb_tokens_completion")
 
+
 @metric_registry.register(
     name="nb_tool_calls",
     description="Number of tools that has been called for the generation",
     metric_type="ops",
-    require=["query"],
+    require=["output"],
 )
 def nb_tool_calls_metric(output, *args, **kwargs):
     metadata = kwargs["metadata"]
@@ -34,7 +35,7 @@ def nb_tool_calls_metric(output, *args, **kwargs):
     name="generation_time",
     description="The time to generate the answer/output",
     metric_type="ops",
-    require=["query"],
+    require=["output"],
 )
 def generation_time_metric(output, *args, **kwargs):
     metadata = kwargs["metadata"]
