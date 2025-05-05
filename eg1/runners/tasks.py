8e0a162f737a5e8b0d83731234df40985fd970b1
dtrckd <dtrckd@gmail.com>
2025-05-05 03:31:35 +0200

    fix(tasks): empty query

--- eg1/runners/tasks.py
+++ eg1/runners/tasks.py
@@ -46,7 +46,8 @@ def generate_answer(message: dict, mcp_bridge: MCPBridgeClient | None):
             tools = mcp_bridge.tools2openai(_tools)
             sampling_params_plus["tools"] = tools
 
-        query = "\n\n".join([model.prelude_prompt, msg.query]) if model.prelude_prompt else (msg.query or "")
+        query = msg.query or ""
+        query = "\n\n".join([model.prelude_prompt, query]) if model.prelude_prompt else query
         answer = None
         error_msg = None
         try:
