1d63b68ef75459dcf1300aa6d355906c1596d61e
Audrey_CLEVY <audrey_clevy@MacBook-Air-de-Audrey-CLEVY.local>
2025-07-10 13:11:37 +0200

    change name files

--- eg1/api/base.py
+++
@@ -1,2 +0,0 @@
-from eg1.api.models import *
-
--- eg1/api/security.py
+++
@@ -1,8 +0,0 @@
-from fastapi import Header, HTTPException
-
-from eg1.api.config import ADMIN_TOKENS
-
-
-async def admin_only(x_eg1_admin: str = Header(default=None)):
-    if ADMIN_TOKENS and x_eg1_admin not in ADMIN_TOKENS:
-        raise HTTPException(status_code=401, detail="Unauthorized: Please contact an admin to perform this request.")
