023b42ef2533d084497c4efb1c245cd33fedadac
AudreyCLEVY <audrey.clevy@mail.numerique.gouv.fr>
2025-07-17 07:06:02 +0200

    Feat/logo (#178)
    * add license
    
    * add logo
    
    ---------
    
    Co-authored-by: Audrey_CLEVY <audrey_clevy@MacBook-Air-de-Audrey-CLEVY.local>
    Co-authored-by: Audrey_CLEVY <audrey_clevy@airdeaureyclevy.home>

--- evalap/ui/demo_streamlit/views/home.py
+++ evalap/ui/demo_streamlit/views/home.py
@@ -1,15 +1,23 @@
 import streamlit as st
 from routes import ROUTES
+from PIL import Image
 
-st.title("Welcome to EvalAP")
+logo_path = "evalap/ui/demo_streamlit/static/images/EvalAP_logo.png"  
+logo = Image.open(logo_path)
 
+col1, col2 = st.columns([4, 2])
 
-st.write("What is on the menu:")
+with col1:
+    st.title("Welcome to EvalAP")
+    st.write("What is on the menu :")
+    for route in ROUTES:
+        if route["id"] in ["home"] or route.get("is_hidden"):
+            continue
+        st.page_link(
+            route["path"],
+            label=f'{route["title"]}: {route["description"]}',
+            icon=route["icon"]
+        )
 
-for route in ROUTES:
-    if route["id"] in ["home"] or route.get("is_hidden"):
-        continue
-
-    st.page_link(
-        route["path"], label=f'{route["title"]}: {route["description"]}', icon=route["icon"]
-    )
+with col2:
+    st.image(logo, use_container_width=True)
