596f6d8d285c6fa8e0698d854c7d504fe3ffc447
dtrckd <dtrckd@gmail.com>
2025-07-17 08:42:42 +0200

    codefactor: format home img

--- evalap/ui/demo_streamlit/views/home.py
+++ evalap/ui/demo_streamlit/views/home.py
@@ -2,18 +2,18 @@ import os
 from PIL import Image
 import streamlit as st
 from routes import ROUTES
+from pathlib import Path
 
-def get_logo_path(filename="evalap_logo.png"):
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    logo_path = os.path.join(script_dir, '..', 'static', 'images', filename)
-    return os.path.normpath(logo_path)
 
-logo_path = get_logo_path()  # Tu peux adapter le nom si besoin
+def get_logo(filename="evalap_logo.png"):
+    img_path = Path(__file__).resolve().parents[1] / "static" / "images"
+    logo_path = img_path / filename
+    if os.path.exists(logo_path):
+        logo = Image.open(logo_path)
+    else:
+        logo = Image.new("RGBA", (200, 100), (255, 255, 255, 0))
+    return logo
 
-if os.path.exists(logo_path):
-    logo = Image.open(logo_path)
-else:
-    logo = Image.new("RGBA", (200, 100), (255, 255, 255, 0))  
 
 col1, col2 = st.columns([4, 2])
 
@@ -23,11 +23,7 @@ with col1:
     for route in ROUTES:
         if route["id"] in ["home"] or route.get("is_hidden"):
             continue
-        st.page_link(
-            route["path"],
-            label=f'{route["title"]}: {route["description"]}',
-            icon=route["icon"]
-        )
+        st.page_link(route["path"], label=f"{route['title']}: {route['description']}", icon=route["icon"])
 
 with col2:
-    st.image(logo, use_container_width=True)
+    st.image(get_logo(), use_container_width=True)
