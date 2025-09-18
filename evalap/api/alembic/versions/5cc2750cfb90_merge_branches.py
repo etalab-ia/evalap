7566c70370eaae2309460b8a25e56ef9e31d1560
Luis Arias <luis@refugies.info>
2025-09-18 14:49:05 +0200

    chore: restore alambic versions scripts and exclude from formatting and linting

--- evalap/api/alembic/versions/5cc2750cfb90_merge_branches.py
+++ evalap/api/alembic/versions/5cc2750cfb90_merge_branches.py
@@ -5,12 +5,15 @@ Revises: a8816ddd6dfd, eee3dc59f461
 Create Date: 2024-12-11 20:02:55.960060
 
 """
-
 from typing import Sequence, Union
 
+from alembic import op
+import sqlalchemy as sa
+
+
 # revision identifiers, used by Alembic.
-revision: str = "5cc2750cfb90"
-down_revision: Union[str, None] = ("a8816ddd6dfd", "eee3dc59f461")
+revision: str = '5cc2750cfb90'
+down_revision: Union[str, None] = ('a8816ddd6dfd', 'eee3dc59f461')
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
